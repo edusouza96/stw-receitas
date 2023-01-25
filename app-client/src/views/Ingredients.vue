d48c5a17fb56005d45b1d87fbcb47e94d0af8055
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2023-01-25 17:36:16 -0300

    APP - Iniciando Projeot Front end

---
+++ app-client/src/views/Ingredients.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="ingredients">
+        <div class="table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Descrição</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="ingredient in ingredients" :key="ingredient.id">
+                        <td>{{ ingredient.id }}</td>
+                        <td>{{ ingredient.description }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .ingredients {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
+<script>
+export default {
+    data() {
+        return {
+            ingredients: [],
+        };
+    },
+    created() {
+        this.axios
+            .get("http://localhost:8000/api/ingredientes")
+            .then((response) => {
+                if (response.data.success) {
+                    this.ingredients = response.data.data;
+                }
+            });
+    },
+};
+</script>
